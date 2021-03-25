637cc358ec45e55289c8ebafa1fba7a4e74eb3c6
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-25 11:36:03 +0100

    5&6

---
+++ ASP_NET_Inlämningsuppgift_Facit/Pages/JoinEvent.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP_NET_Inlämningsuppgift_Facit.Data;
+using ASP_NET_Inlämningsuppgift_Facit.Models;
+
+namespace ASP_NET_Inlämningsuppgift_Facit.Pages
+{
+    public class JoinEventModel : PageModel
+    {
+        private readonly ASP_NET_Inlämningsuppgift_Facit.Data.EventDbContext _context;
+
+        public JoinEventModel(ASP_NET_Inlämningsuppgift_Facit.Data.EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ ASP_NET_Inlämningsuppgift_Facit/Pages/MyEvents.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP_NET_Inlämningsuppgift_Facit.Data;
+using ASP_NET_Inlämningsuppgift_Facit.Models;
+
+namespace ASP_NET_Inlämningsuppgift_Facit.Pages
+{
+    public class MyEventsModel : PageModel
+    {
+        private readonly ASP_NET_Inlämningsuppgift_Facit.Data.EventDbContext _context;
+
+        public MyEventsModel(ASP_NET_Inlämningsuppgift_Facit.Data.EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            var attendee = await _context.Attendees.Include(a => a.Events).FirstOrDefaultAsync();
+            Event = attendee.Events;
+        }
+    }
+}
