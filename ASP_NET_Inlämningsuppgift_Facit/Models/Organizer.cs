aabed77efbb12c1eb0a898d492f285458dabd0e7
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-25 10:46:04 +0100

    Add project files.

---
+++ ASP_NET_Inlämningsuppgift_Facit/Models/Organizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Inlämningsuppgift_Facit.Models
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<Event> Events { get; set; }
+    }
+}
